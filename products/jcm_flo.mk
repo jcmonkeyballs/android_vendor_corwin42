446919ef7b80e8670fde6513b61a312516b30034
jcmonkeyballs <acsutherland@gmail.com>
2014-07-03 07:15:51 -0400

    Update jcm_flo.mk

--- products/jcm_flo.mk
+++ products/jcm_flo.mk
@@ -11,11 +11,11 @@ PRODUCT_DEVICE := flo
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.2 KOT49H 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/flo:4.4.4/KTU84P/1227136:user/release-keys' PRIVATE_BUILD_DESC="razor-user 4.4.4 KTU84P 1227136 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
 
 # boot animation
 PRODUCT_COPY_FILES += \
-    vendor/corwin42/proprietary/flo/system/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/jcm/proprietary/flo/system/media/bootanimation.zip:system/media/bootanimation.zip
